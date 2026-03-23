8a5b0d0fce44342e7863a92cf78047e925d2fd88
Astha Negi <75171246+AsthaNegi@users.noreply.github.com>
2026-03-23 12:07:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+  let cost=0;
+
+ while(arr.length>1){
+        arr.sort((a,b)=>a-b);
+        let sum=arr[0]+arr[1];
+        cost+=sum;
+        arr.splice(0,2);
+        arr.push(sum);
+}
+ return cost;
 }
 
 module.exports=mincost;
